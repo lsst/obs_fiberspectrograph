8b21fde0088135eacecb37a6049cd5acfbdd446e
Agnès Ferté <ferte@slac.stanford.edu>
2024-01-30 17:05:06 -0800

    Update default band name.

--- python/lsst/obs/fiberspectrograph/filters.py
+++ python/lsst/obs/fiberspectrograph/filters.py
@@ -1,5 +1,5 @@
 from lsst.obs.base import FilterDefinition, FilterDefinitionCollection
 
 FIBER_SPECTROGRAPH_FILTER_DEFINITIONS = FilterDefinitionCollection(
-    FilterDefinition(band="empty", physical_filter="empty"),
+    FilterDefinition(band="white", physical_filter="empty"),
 )
